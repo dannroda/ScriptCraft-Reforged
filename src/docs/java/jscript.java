19843b2b49d2f20a485cb2d4de4bb61e53f8f69e
Daniel Rodriguez Aires <dannroda@gmail.com>
2025-03-21 12:45:03 -0300

    WIP Update Bukkit helper and documentation generator

--- src/docs/java/jscript.java
+++ src/docs/java/jscript.java
@@ -1,22 +1,45 @@
-import javax.script.*;
-import java.io.FileReader;
-import net.canarymod.api.inventory.ItemType;
+import java.io.*;
+import java.nio.charset.StandardCharsets;
 
-public class jscript
-{
-    public static void main(String[] args) throws Exception
-    {
-        ScriptEngineManager factory = new ScriptEngineManager();
-        ScriptEngine engine = factory.getEngineByName("JavaScript");
-        java.io.File file = new java.io.File(args[0]);
-        engine.put("engine",engine);
-        engine.put("args",args);
-	try { 
-	    engine.put("cmItemTypeClass",Class.forName("net.canarymod.api.inventroy.ItemType"));
-	}catch(Exception e){
-	}
-        FileReader fr = new java.io.FileReader(file);
-        engine.eval(fr);
-        fr.close();
+import org.apache.commons.io.FileUtils;
+import org.graalvm.polyglot.*;
+
+public class jscript {
+    public static void main(String[] args) throws Exception {
+
+        String filePath = args[0];
+
+        File errFile = new File(args[args.length - 1]);
+        System.setErr(new PrintStream(errFile));
+
+        try (Context context = Context.newBuilder("js")
+                .allowHostAccess(HostAccess.ALL)
+                .allowHostClassLookup(className -> true)
+                .allowAllAccess(true)
+                .allowNativeAccess(true)
+                .allowExperimentalOptions(true)
+                .option("js.nashorn-compat", "true")
+                .build()) {
+            Value bindings = context.getBindings("js");
+            bindings.putMember("engine", context);
+            bindings.putMember("args", args);
+            bindings.putMember("lib", new File(args[2]));
+            Value script = context.eval("js", FileUtils.readFileToString(new File(filePath), StandardCharsets.UTF_8));
+            // Debug generation output to file
+            try (FileWriter fileWriter = new FileWriter("target/output_debug.txt", true);
+                 BufferedWriter bufferedWriter = new BufferedWriter(fileWriter)) {
+                bufferedWriter.write("------");
+                bufferedWriter.newLine();
+
+                for (String str : args) {
+                    bufferedWriter.write(str);
+                    bufferedWriter.newLine();
+
+                }
+            }
+
+        }
+
+
     }
 }
